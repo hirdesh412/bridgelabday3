595084cab7f02c32786f8cb9f62d4c0928a1e53d
hirdesh412 <122356033+hirdesh412@users.noreply.github.com>
2025-07-01 12:54:13 +0530

    Add files via upload

---
+++ Methods_Functions.java
@@ -0,0 +1,26 @@
+public class Methods_Functions {
+    //parameterized
+//    public static void greetUser(String name) { // 'name' is a parameter of type String
+//        System.out.println("Hello, " + name + "!");
+//    }
+//    public static void addNumbers(int num1, int num2) { // 'num1' and 'num2' are parameters of type int
+//        System.out.println(num1+num2);
+//    }
+
+    //Non-paramerterized
+//    public void printMessage() {
+//        System.out.println("Hello, Guys!");
+//    }
+
+ //   public static void main(String[] args) {
+//        greetUser("Harsh");
+//        addNumbers(10,20);
+
+//        Methods_Functions obj = new Methods_Functions();
+//        obj.printMessage();
+
+
+
+    }
+
+}
